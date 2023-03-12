d416ceb334b8362d94f2e973241c8cf2c64f3876
Werenfeldt <josefine.werenfeldt@hotmail.com>
2023-03-12 11:58:41 +0100

    connected to OpenAIChatGPT

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Interfaces;
 using Presentation.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddOpenAIService().BuildServiceProvider().GetRequiredService<IOpenAIService>();
 
 var app = builder.Build();
 
