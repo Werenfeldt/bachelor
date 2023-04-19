6c1b0c14f46104740c6ccce12e9775b306749808
frederikmnielsen <90323626+frederikmnielsen@users.noreply.github.com>
2023-04-19 16:37:55 +0200

    new prompt with better results (#32)

--- ServiceLayer/IService/ITranslationService.cs
+++ ServiceLayer/IService/ITranslationService.cs
@@ -2,6 +2,6 @@ namespace ServiceLayer;
 
 public interface ITranslationService
 {
-    Task<DocumentationDTO> TranslateTestfile(TestFileDTO testFile, string prompt = "Translate the following cypress script into steps in natural language: ");
+    Task<DocumentationDTO> TranslateTestfile(TestFileDTO testFile, string prompt = "Translate the following cypress script into steps in natural language such that a QA tester can execute the test case in a browser: ");
     Task<Response> UpdateDocumentation(UpdateDocumentationDTO documentation);
 }
