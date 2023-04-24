5aab650cd8cf7de5f0c65e22462dee3276623610
JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
2023-04-24 14:44:38 +0200

    de fleste til projekt service

--- ServiceLayer.Tests/ProjectServiceTests.cs
+++ ServiceLayer.Tests/ProjectServiceTests.cs
@@ -5,53 +5,159 @@ public class ProjectRepositoryTests : ContextSetup
     [Fact]
     public async Task CreateProject_given_CreateProjectDTO_and_authtoken_returns_ProjectDTO()
     {
-        var createProjectDTO = new CreateProjectDTO()
-        {
-            Title = "Test",
-            GitUrl = "https://github.com/JeppeKorgaard98/bachelortest",
-            Description = "I am a description",
-            UserId = new Guid("596bd00e-8699-4183-850f-14dc879bf9d8"),
-            TestFileToBeCreatedDTOs = new List<CreateTestFileDTO>()
-        };
-        var created = await _serviceManager.ProjectService.CreateProjectAsync(createProjectDTO, _testRepoGitKey);
+        var user = await CreateTestUser();
+        var project = await CreateTestProject1(user.Id);
 
-        Console.WriteLine(created.ToString());
-
-        Assert.Equal(createProjectDTO.Title, created.title);
-        Assert.Equal(createProjectDTO.GitUrl, created.gitUrl);
-        Assert.Equal(createProjectDTO.Description, created.description);
-        Assert.Equal(DateTime.UtcNow, created.createdDate, precision: TimeSpan.FromSeconds(5));
-
-
+        Assert.Equal("Test1", project.title);
+        Assert.Equal("https://github.com/JeppeKorgaard98/bachelortest", project.gitUrl);
+        Assert.Equal("I am a description", project.description);
+        Assert.Equal(DateTime.UtcNow, project.createdDate, precision: TimeSpan.FromSeconds(5));
     }
 
     [Fact]
-    public async Task CreateProject_given_CreateProjectDTO_and_username_password_returns_projectDTO()
+    public async Task CreateProject_given_CreateProjectDTO_and__wrong_authtoken_returns_exception()
     {
+        var user = await CreateTestUser();
         var createProjectDTO = new CreateProjectDTO()
         {
-            Title = "Test",
+            Title = "Test1",
             GitUrl = "https://github.com/JeppeKorgaard98/bachelortest",
             Description = "I am a description",
-            UserId = new Guid("596bd00e-8699-4183-850f-14dc879bf9d8"),
+            UserId = user.Id,
             TestFileToBeCreatedDTOs = new List<CreateTestFileDTO>()
         };
-        string username = "Jens";
-        string password = "Jens123";
-        var created = await _serviceManager.ProjectService.CreateProjectAsync(createProjectDTO, username, password);
 
-        Console.WriteLine(created.ToString());
-
-        Assert.Equal(createProjectDTO.Title, created.title);
-        Assert.Equal(createProjectDTO.GitUrl, created.gitUrl);
-        Assert.Equal(createProjectDTO.Description, created.description);
-        Assert.Equal(DateTime.UtcNow, created.createdDate, precision: TimeSpan.FromSeconds(5));
+        await Assert.ThrowsAsync<Octokit.AuthorizationException>(async () => await _serviceManager.ProjectService.CreateProjectAsync(createProjectDTO, "wrong key"));
     }
 
     [Fact]
-    public async Task LoadProjects_given_userId_returns_projectDTO_list()
+    public async Task LoadProjects_given_userId_returns_list_of_ProjectDTO()
     {
+        var user = await CreateTestUser();
 
+        var project1 = await CreateTestProject1(user.Id);
+        var project2 = await CreateTestProject2(user.Id);
+
+        var resultList = await _serviceManager.ProjectService.LoadProjectsAsync(user.Id);
+
+        Assert.Equal("Test1", resultList[0].title);
+        Assert.Equal("https://github.com/JeppeKorgaard98/bachelortest", resultList[0].gitUrl);
+        Assert.Equal("I am a description", resultList[0].description);
+        Assert.Equal("Test2", resultList[1].title);
+        Assert.Equal("https://github.com/cypress-io/cypress-realworld-app", resultList[1].gitUrl);
+        Assert.Equal("I am a description", resultList[1].description);
+        Assert.Equal(2, resultList.Count);
     }
 
+    [Fact]
+    public async Task LoadProjects_given_wrong_userId_throws_exception()
+    {
+        await Assert.ThrowsAsync<System.NullReferenceException>(async () => await _serviceManager.ProjectService.LoadProjectsAsync(new Guid()));
+    }
+
+    [Fact]
+    public async Task LoadTestFileById_given_id_returns_TestFileDTO()
+    {
+        var testFile = await _serviceManager.ProjectService.LoadTestFileByIdAsync(new Guid("ee61a729-a960-467a-bdc1-1d7184ee7346"));
+
+        Assert.Equal("TestFile 1", testFile.Name);
+        Assert.Equal("somePath", testFile.Path);
+        Assert.Equal("Jeg er et script content", testFile.Content);
+    }
+
+    [Fact]
+    public async Task LoadTestFileById_given_wrong_id_throws_exception()
+    {
+        await Assert.ThrowsAsync<TestFileDoesNotExistException>(async () => await _serviceManager.ProjectService.LoadTestFileByIdAsync(new Guid()));
+    }
+
+    [Fact]
+    public async Task LoadTestFilesForProject_given_id_returns_list_of_TestFileDTO()
+    {
+        var user = await CreateTestUser();
+        var project = await CreateTestProject1(user.Id);
+
+        var testFiles = await _serviceManager.ProjectService.LoadTestFilesForProjectAsync(project.Id);
+
+        Assert.Equal(2, testFiles.Count);
+    }
+
+    [Fact]
+    public async Task LoadTestFilesForProject_given_wrong_id_returns_empty_list()
+    {
+        Assert.Empty(await _serviceManager.ProjectService.LoadTestFilesForProjectAsync(new Guid()));
+    }
+
+    [Fact]
+    public async Task LoadSideBar_given_user_id_returns_list_of_ProjectWithTestFilesDTO()
+    {
+        var user = await CreateTestUser();
+        var project = await CreateTestProject1(user.Id);
+
+        var projectWithTestFilesDTO = await _serviceManager.ProjectService.LoadSideBar(user.Id);
+
+        Assert.Equal(project.Id, projectWithTestFilesDTO[0].Id);
+        Assert.Equal(project.title, projectWithTestFilesDTO[0].Title);
+        Assert.Equal(2, projectWithTestFilesDTO[0].testfiles.Count());
+    }
+
+    [Fact]
+    public async Task LoadSideBar_given_wrong_id_throws_exception()
+    {
+        //TODO
+    }
+
+    [Fact]
+    public async Task LoadDocumentationByTestFilesId_given_id_returns_DocumentationDTO()
+    {
+        //TODO
+    }
+
+    [Fact]
+    public async Task LoadDocumentationByTestFilesId_given_wrong_id_throws_exception()
+    {
+        await Assert.ThrowsAsync<System.NullReferenceException>(async () => await _serviceManager.ProjectService.LoadSideBar(new Guid()));
+    }
+
+
+
+
+    // UTIL
+
+    private async Task<UserDTO> CreateTestUser()
+    {
+        var createUserDTO = new CreateUserDTO()
+        {
+            Name = "Alice",
+            Email = "Alice@gmail.com",
+            Password = "Alice123"
+        };
+        return await _serviceManager.LoginService.CreateNewUser(createUserDTO);
+    }
+
+    private async Task<ProjectDTO> CreateTestProject1(Guid userId)
+    {
+        var createProjectDTO = new CreateProjectDTO()
+        {
+            Title = "Test1",
+            GitUrl = "https://github.com/JeppeKorgaard98/bachelortest",
+            Description = "I am a description",
+            UserId = userId,
+            TestFileToBeCreatedDTOs = new List<CreateTestFileDTO>()
+        };
+        return await _serviceManager.ProjectService.CreateProjectAsync(createProjectDTO, _testRepoGitKey);
+    }
+
+    private async Task<ProjectDTO> CreateTestProject2(Guid userId)
+    {
+        var createProjectDTO = new CreateProjectDTO()
+        {
+            Title = "Test2",
+            GitUrl = "https://github.com/cypress-io/cypress-realworld-app",
+            Description = "I am a description",
+            UserId = userId,
+            TestFileToBeCreatedDTOs = new List<CreateTestFileDTO>()
+        };
+        return await _serviceManager.ProjectService.CreateProjectAsync(createProjectDTO, _testRepoGitKey);
+    }
 }
--- ServiceLayer/IService/IProjectService.cs
+++ ServiceLayer/IService/IProjectService.cs
@@ -3,17 +3,11 @@ namespace ServiceLayer;
 public interface IProjectService
 {
     Task<ProjectDTO> CreateProjectAsync(CreateProjectDTO projectDTO, string tokenAuth);
-
     Task<ProjectDTO> CreateProjectAsync(CreateProjectDTO projectDTO, string username, string password);
-
     Task<List<ProjectDTO>> LoadProjectsAsync(Guid userId);
-
     Task<TestFileDTO> LoadTestFileByIdAsync(Guid testFileId);
     Task<List<TestFileDTO>> LoadTestFilesForProjectAsync(Guid projectId);
-
     Task<List<ProjectWithTestFilesDTO>> LoadSideBar(Guid userId);
-
     Task<DocumentationDTO> LoadDocumentationByTestFilesIdAsync(Guid testFileId);
-
     Task<Response> DeleteProject(Guid projectId);
 }
