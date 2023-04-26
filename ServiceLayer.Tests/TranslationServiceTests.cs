7f93ad199ccc3e879a03f0a3fba5979d6a564cf1
JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
2023-04-26 10:29:36 +0200

    bang bagn

--- ServiceLayer.Tests/TranslationServiceTests.cs
+++ ServiceLayer.Tests/TranslationServiceTests.cs
@@ -7,4 +7,45 @@ public class TranslationServiceTests : ContextSetup
     {
 
     }
+
+
+
+    // UTIL
+
+    private async Task<UserDTO> CreateTestUser()
+    {
+        var createUserDTO = new CreateUserDTO()
+        {
+            Name = "Alice",
+            Email = "Alice@gmail.com",
+            Password = "Alice123"
+        };
+        return await _serviceManager.LoginService.CreateNewUser(createUserDTO);
+    }
+
+    private async Task<ProjectDTO> CreateTestProject1(Guid userId)
+    {
+        var createProjectDTO = new CreateProjectDTO()
+        {
+            Title = "Test1",
+            GitUrl = "https://github.com/JeppeKorgaard98/bachelortest",
+            Description = "I am a description",
+            UserId = userId,
+            TestFileToBeCreatedDTOs = new List<CreateTestFileDTO>()
+        };
+        return await _serviceManager.ProjectService.CreateProjectAsync(createProjectDTO, _testRepoGitKey);
+    }
+
+    private async Task<ProjectDTO> CreateTestProject2(Guid userId)
+    {
+        var createProjectDTO = new CreateProjectDTO()
+        {
+            Title = "Test2",
+            GitUrl = "https://github.com/cypress-io/cypress-realworld-app",
+            Description = "I am a description",
+            UserId = userId,
+            TestFileToBeCreatedDTOs = new List<CreateTestFileDTO>()
+        };
+        return await _serviceManager.ProjectService.CreateProjectAsync(createProjectDTO, _testRepoGitKey);
+    }
 }
