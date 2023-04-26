99263cd88970da8f0f829d58cb1b741e04698819
JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
2023-04-26 14:24:23 +0200

    final

--- ServiceLayer.Tests/ContextSetup.cs
+++ ServiceLayer.Tests/ContextSetup.cs
@@ -23,16 +23,20 @@ public abstract class ContextSetup
         configBuilder.AddUserSecrets<ContextSetup>();
         var configuration = configBuilder.Build();
 
+
+        //These variables are set in the Github Environment to run GitHub Actions.
+        //If the tests are run locally all the values will be null.
         string? key = Environment.GetEnvironmentVariable("OpenAIServiceOptions");
         _defaultGitKey = Environment.GetEnvironmentVariable("GithubIntegrationToken");
         _testRepoGitKey = Environment.GetEnvironmentVariable("GithubTestToken");
 
         if (key == null || _defaultGitKey == null || _testRepoGitKey == null)
         {
+            //Make sure all theese values are declared locally as user secrets,
+            //otherwise the keys will be null and an exception will be thrown when trying to use the keys.
             key = configuration.GetValue<string>("OpenAIServiceOptions:ApiKey");
             _defaultGitKey = configuration.GetValue<string>("APIToken:GithubIntegrationToken");
             _testRepoGitKey = configuration.GetValue<string>("APIToken:GithubTestToken");
-            Console.WriteLine("hej");
         }
 
         var openAiService = new OpenAIService(new OpenAiOptions()
